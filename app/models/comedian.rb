ef205f1acb6cb5a0914d235de1f321855f4f9344
lilwillifo <margaretwilliford4@gmail.com>
2018-03-16 10:34:36 -0600

    user can see list of specials

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,4 @@
 class Comedian < ActiveRecord::Base
   validates :name, presence: true
+  has_many :specials
 end
--- spec/views/user_visits_comedians_page_spec.rb
+++ spec/views/user_visits_comedians_page_spec.rb
@@ -1,5 +1,5 @@
 describe 'User visits comedians page' do
-  it 'sees all merchants' do
+  it 'sees all merchants names and ages' do
     Comedian.create(name: 'Amy Poehler', age: 36)
     Comedian.create(name: 'Tina Fey', age: 38)
     visit '/comedians'
@@ -8,5 +8,15 @@ describe 'User visits comedians page' do
     expect(page).to have_content(36)
     expect(page).to have_content('Tina Fey')
     expect(page).to have_content(38)
+  end
+  it 'sees all comedians specials' do
+    Comedian.create(name: 'Amy Poehler', age: 36)
+    Special.create(name: 'Parks and Rec', comedian_id: 1)
+    Special.create(name: 'SNL', comedian_id: 1)
+
+    visit '/comedians'
+
+    expect(page).to have_content('Parks and Rec')
+    expect(page).to have_content('SNL')
   end
 end
